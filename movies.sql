77578ecacadb0afe29e6290e1eed0fa359282626
aryanshetty12 <80276376+aryanshetty12@users.noreply.github.com>
2022-09-25 17:29:30 +0530

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -10,31 +10,33 @@ CREATE TABLE movies(
 
 -- Inserting the values into 'movies' table
 INSERT INTO movies VALUES('Bahubali The Conclusion','Prabhas','Anushka Shetty',2016,'S Rajmouli');
-INSERT INTO movies VALUES('KGF Chapter 1','Yash','Srinidhi Shetty',2018,'Prashanth Neel');
-INSERT INTO movies VALUES('Shersha','Siddharth','Kiara',2020,'Vishnuvardhan');
-INSERT INTO movies VALUES('Kotigobba 3','Sudeep','Ashika',2021,'Shiva Karthik');
-INSERT INTO movies VALUES('Arjun Reddy','Vijay Devarkonda','Shalini Pandey',2017,'Sandeep Reddy');
+INSERT INTO movies VALUES('Casino Royale','Daniel Craig','Eva Green','2006,'Martin Campbell');
+INSERT INTO movies VALUES('Do Revenge','Austin Abrams','Camila Mendes','2022,'Jennifer Robinson');
+INSERT INTO movies VALUES('Mank','Gary Oldman','lily Colins','2020','David Fincher');
+INSERT INTO movies VALUES('Day Shift','Jamie Foxx','Natasha Bordizzo','2022','J.J.Perry');
+INSERT INTO movies VALUES('Shamshera','Ranbir Kapoor','Vaani Kapoor','2022','Karan Malhotra');
+INSERT INTO movies VALUES('Singham','Ajay Devgn','Kajal Aggarwal','2011','Rohit Shetty');
 
 
 -- 1. Retrieving all the attributes from 'movies' table
 SELECT * FROM movies;
 
 
--- 2. Retrieving all the attributes from 'movies' table based on 'lead_actor'
-SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE lead_actor='Yash';
+-- 2. Retrieving all the attributes from 'movies' table whose 'year_of_release' between 2010,2020
+ SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE year_of_release BETWEEN 2010 AND 2020;
 
 
--- 3. Retrieving 'movies_name' from 'movies' table based on 'lead_actor','actress','year_of_release'
-SELECT movie_name FROM movies WHERE lead_actor='Siddharth' AND actress='Kiara' AND year_of_release=2020;
+-- 3.Retrieving all the 'movie_name','director_name' from 'movies' table that are not directed by ''David Fincher'
+ SELECT movie_name,director_name FROM movies WHERE director_name!='David Fincher';
 
 
--- 4. Retrieving all the attributes from 'movies' table whose 'year_of_release' between 2017,2020
-SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE year_of_release BETWEEN 2017 AND 2020;
+-- 4. Retrieving all the attributes from 'movies' table based on 'lead_actor'
+SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE lead_actor='Ranbir Kapoor';
 
 
--- 5. Sort the values of tables based on 'year_of_release' 
+-- 5. Retrieving 'movies_name' from 'movies' table based on 'lead_actor','actress','year_of_release'
+ SELECT movie_name FROM movies WHERE lead_actor='Jamie Foxx' AND actress='Natasha Bordizzo' AND year_of_release=2022;
+
+
+-- 6. Sort the values of tables based on 'year_of_release'
 SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies ORDER BY year_of_release;
-
-
--- 6. Retrieving all the 'movie_name','director_name' from 'movies' table that are not directed by 'Sandeep Reddy'
-SELECT movie_name,director_name FROM movies WHERE director_name!='Sandeep Reddy';
